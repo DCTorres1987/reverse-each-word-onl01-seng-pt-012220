c581aaee7358ff94806170807f27a93e63457ca5
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-28 00:56:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,7 +10,7 @@ end
 def reverse_each_word(sentence)
   original_string = sentence.split(" ")
   reverse_array = []
-  original_string.each do |sentence|
+  original_string.collect do |sentence|
     reverse_array << sentence.reverse
   end
    reverse_array.join(" ")
