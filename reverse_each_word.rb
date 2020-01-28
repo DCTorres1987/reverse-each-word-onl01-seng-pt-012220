079726215776116f49226d704c70e2587a1b52c9
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-01-28 00:41:21 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   original_string = sentence.split(" ")
   original_string.each do |phrase|
-    puts reverse_array << phrase.reverse
+    reverse_array << phrase.reverse
   end
    reverse_array
 end
